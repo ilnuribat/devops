ee70f7b776412202b04c7dc5d14a7e149620cd41
ilnuribat <ilnuribat@gmail.com>
2018-10-08 19:34:35 +0300

    move decrypted files to gitignore

--- vault.sh
+++ vault.sh
@@ -5,20 +5,15 @@ files=(gitrepo.pem priv.pem roles/deploy_app/templates/env.secret)
 if [[ $1 = "decrypt" ]]; then
   for file in "${files[@]}"
   do
-    ansible-vault --vault-password-file=.ansible_vault_pass decrypt "$file"
+    ansible-vault --vault-password-file=.ansible_vault_pass \
+      decrypt "$file.vaulted" \
+      --output "$file"
   done
 else
   for file in "${files[@]}"
   do
-    ansible-vault --vault-password-file=.ansible_vault_pass encrypt "$file"
+    ansible-vault --vault-password-file=.ansible_vault_pass \
+      encrypt "$file" \
+      --output "$file.vaulted"
   done
 fi;
-
-
-
-
-# ansible-vault --vault-password-file=.ansible_vault_pass \
-#   encrypt \
-#   gitrepo.pem \
-#   priv.pem \
-#   inventory/group_vars/*

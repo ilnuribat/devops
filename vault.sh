fa17c32082efe46b2ccad7e8800b70390666485d
ilnuribat <ilnuribat@gmail.com>
2018-10-08 19:23:24 +0300

    use ansible vault

---
+++ vault.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+files=(gitrepo.pem priv.pem)
+
+if [[ $1 = "decrypt" ]]; then
+  for file in "${files[@]}"
+  do
+    ansible-vault --vault-password-file=.ansible_vault_pass decrypt "$file"
+  done
+else
+  for file in "${files[@]}"
+  do
+    ansible-vault --vault-password-file=.ansible_vault_pass encrypt "$file"
+  done
+fi;
+
+
+
+
+# ansible-vault --vault-password-file=.ansible_vault_pass \
+#   encrypt \
+#   gitrepo.pem \
+#   priv.pem \
+#   inventory/group_vars/*
